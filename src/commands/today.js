119c2d18a987d01dacfa8c9b219aefdd7d51bc18
Vladislav Atakhanov <eloh1mZee@gmail.com>
2024-01-30 09:24:24 +0600

    fix: /today delete button

--- src/commands/today.js
+++ src/commands/today.js
@@ -1,7 +1,10 @@
 import { getDayExpensesSum } from "../database/index.js"
+import { RemoveButton } from "./utils.js"
 
 /** @type {import("../bot/@types.ts").MessageHandler} */
 export const todayCommand = async (message, bot) => {
     const todaySum = await getDayExpensesSum(message.chat.id, new Date())
-    bot.sendMessage(message.chat.id, `Траты за сегодня: ${todaySum}`)
+    bot.sendMessage(message.chat.id, `Траты за сегодня: ${todaySum}`, {
+        reply_markup: RemoveButton(message.message_id),
+    })
 }
